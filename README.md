87a0bfd3b493a8bdfa4b0ecfb708e37aab2c1848
epistemex <70324091+epistemex@users.noreply.github.com>
2020-12-24 01:57:45 -0600

    baudcat

---
+++ README.md
@@ -0,0 +1,42 @@
+baudcat
+=======
+
+A simple buffered cat command with BAUD rate simulation.
+
+Cross-platform for terminal emulators.
+
+Usage
+-----
+
+Examples:
+
+    baudcat textfile.txt                # output textfile at default 2,400 BAUD
+    baudcat --baud 300 textfile.txt     # output textfile at 300 BAUD
+    baudcat -C anim.vt                  # hide cursor before outputting, restore at end
+    baudcat file.txt --fps 30 -b 9600   # reduce frame rate
+    cat textfile.txt | baudcat -        # pipe and use STDIN (-) as input file
+
+Use the option `--help` or `-h` to see more information.
+
+Notes
+-----
+
+The BAUD rate is currently an approximation due to various factors that hasn't been addressed. This
+behavior may or may not change in a future release.
+
+Use the option `--stats` for actual B/s rate.
+
+
+Installation
+------------
+
+Requires Node.js and NPM. Install globally:
+
+    npm i -g https://github.com/epistemex/baudcat
+
+License
+-------
+
+MIT (see the [LICENSE-MIT](./LICENSE-MIT) file for details)
+
+Copyright (c) 2020 Epistemex
---
+++ index.js
@@ -0,0 +1,119 @@
+#!/usr/bin/env node
+/* *********************************************************
+ *
+ *  baudcat
+ *
+ *  Copyright (c) 2020 Epistemex
+ *
+ **********************************************************/
+
+'use strict';
+
+const write = process.stdout.write.bind(process.stdout);
+const error = process.stderr.write.bind(process.stderr);
+
+const options = require('commander')
+  .description('A simple buffered cat command with BAUD rate simulation.\n(c) 2020 Epistemex')
+  .version(require('./package.json').version, '-v, --version')
+  .usage('[options] textfile')
+  .option('-b, --baud <rate>', 'Simulate baud rate [1, 512K].', 2400)
+  .option('-C, --no-cursor', 'Hide cursor while outputting. Restore at end.')
+  .option('--fps <fps>', 'Frames per second [1, 240].', 60)
+  .option('--stats', 'Show stats at the end.')
+  .option('--blocksize <size>', 'Read blocksize, in KB (ignored if STDIN)', 128)
+  .parse(process.argv);
+
+const isStdIn = options.args[ 0 ] === '-';
+const startTime = Date.now();
+
+if ( options.args.length === 1 ) {
+  process.on('exit', () => {
+    resetCursor();
+
+    if ( options.stats && options.args[ 0 ] !== '-' ) {
+      const endTime = Date.now();
+      const size = require('fs').statSync(options.args[ 0 ]).size;
+      const time = (endTime - startTime) / 1000;
+      const bps = size / time;
+
+      write('\x1b[0m\n' + '='.repeat(72) + '\n');
+      write(`Size: ${ size } bytes\n`);
+      write(`Time: ${ time.toFixed(1) } seconds\n`);
+      write(`BAUD: ${ bps.toFixed(1) } B/s (setting: ${ options.baud } BAUD)\n`);
+      write('='.repeat(72) + '\n');
+    }
+  });
+
+  process.on('SIGINT', () => {
+    resetCursor();
+    write('\n');
+    options.stats = false;
+    process.exit();
+  });
+
+  go();
+}
+else options.outputHelp();
+
+// -----------------------------------------------------------------------------
+
+function go() {
+  const fs = require('fs');
+  const { Transform } = require('stream');
+
+  // Sanity checks and clamping
+  const blockSize = Math.max(1, Math.min(options.blocksize, 4194304)) << 10;
+  const baud = Math.max(1, Math.min(524288, options.baud | 0));
+  const fps = Math.max(1, Math.min(240, options.fps | 0));
+
+  let step = 1;
+  let delay;
+  let readStr;
+
+  /**
+   * Will write data in small chunks per frame or char delay per char (baud < fps)
+   * at the rate calculated by `delay` and delta by `step`.
+   * @param options
+   * @constructor
+   */
+  class TDelay extends Transform {
+    constructor(options) {super(options);}
+
+    _transform(chunk, enc, cb) {
+      let i = 0;
+
+      // todo consider improving accuracy by using delta times.
+      setTimeout(function _chunk() {
+        const ref = setTimeout(_chunk, delay);
+        write(chunk.subarray(i, i += step));
+        if ( i >= chunk.length ) {
+          clearTimeout(ref);
+          cb();
+        }
+      }, delay);
+    }
+  }
+
+  // open read stream and add handlers
+  (readStr = isStdIn ? process.stdin : fs.createReadStream(options.args[ 0 ], { highWaterMark: blockSize }))
+    .on('error', () => {error('Could not open file.\n');})
+    .on('end', () => {if ( !isStdIn ) readStr.close();});
+
+  // hide cursor?
+  if ( !options.cursor ) write('\x1b[0m\x1b[?25l');
+
+  if ( baud < fps ) {
+    delay = baud;
+  }
+  else {
+    delay = fps;
+    step = Math.max(1, Math.ceil(baud / fps));
+  }
+
+  delay = Math.max(4, Math.round(1000 / delay));
+  readStr.pipe(new TDelay());
+}
+
+function resetCursor() {
+  if ( !options.cursor ) write('\x1b[0m\x1b[?25h');
+}
